3dd8f561b3af76c4298ccdeb833c8f90dd072fd0
Schoa <phungminhkhoa2006@gmail.com>
2023-01-07 17:01:48 +0700

    link

--- Paddy_Disease/Web/main.js
+++ Paddy_Disease/Web/main.js
@@ -1,1 +1,6 @@
-document.getElementById("knowledge").onclick="location.href=''"
+document.getElementById("tech").onclick="location.href=''";
+document.getElementById("manage").onclick="location.href=''";
+document.getElementById("knowledge").onclick="location.href=''";
+document.getElementById("news").onclick="location.href=''";
+document.getElementById("QnA").onclick="location.href=''";
+document.getElementById("detect").onclick="location.href=''";
