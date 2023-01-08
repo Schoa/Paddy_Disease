1bae52ecd56c9ddb66cac8b037c92442d30629cb
Schoa <phungminhkhoa2006@gmail.com>
2023-01-08 09:46:22 +0700

    tôi không biết phải nói gì?

--- Paddy_Disease/Web/main.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("tech").onclick="location.href=''";
-document.getElementById("manage").onclick="location.href=''";
-document.getElementById("knowledge").onclick="location.href=''";
-document.getElementById("news").onclick="location.href=''";
-document.getElementById("QnA").onclick="location.href=''";
-document.getElementById("detect").onclick="location.href=''";
